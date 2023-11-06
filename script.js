cc6722e0c7cf4a9f6ee8320a10c946f1644bb92e
Jay Singh <learn.jayssingh@gmail.com>
2023-11-06 19:29:52 +0530

    click-drag to draw -instead-of- hovering

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const container = document.querySelector('#container');
 
-for (let j = 0; j < 16; j++) {
-    for (let i = 0; i < 16; i++) {
+// for holding and dragging through the sketchpad to draw
+let isDrawing = false;
+container.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+
+container.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+container.addEventListener('mouseleave', () => {
+    isDrawing = false;
+});
+
+for (let rows = 0; rows < 16; rows++) {
+    for (let columns = 0; columns < 16; columns++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
 
         square.addEventListener('mouseover', function(event) {
-            event.target.style.backgroundColor = "red";
+            if (isDrawing) {
+                event.target.style.backgroundColor = "red";
+            }
         });
     }
 }
