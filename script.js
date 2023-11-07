daa8c67b84a947d6bf08fe4192006cb40af8ed49
Jay Singh <learn.jayssingh@gmail.com>
2023-11-07 21:15:51 +0530

    custom grid creation feature

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#grid-container');
+const customGrid = document.querySelector('#custom-grid');
+let gridSize = 16;
 
 // for holding and dragging through the sketchpad to draw
 let isDrawing = false;
@@ -14,16 +16,51 @@ container.addEventListener('mouseleave', () => {
     isDrawing = false;
 });
 
-for (let rows = 0; rows < 16; rows++) {
-    for (let columns = 0; columns < 16; columns++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
+function createGrid(gridSize) {
+    container.innerHTML = '';
+    for (let rows = 0; rows < parseInt(gridSize); rows++) {
+        for (let columns = 0; columns < parseInt(gridSize); columns++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            container.appendChild(grid);
 
-        square.addEventListener('mouseover', function(event) {
-            if (isDrawing) {
-                event.target.style.backgroundColor = "red";
-            }
-        });
+            const gridDimensions = sideCalculator(gridSize);
+            grid.style.width = gridDimensions+'px';
+            grid.style.height = gridDimensions+'px';
+
+            grid.addEventListener('mouseover', function (event) {
+                if (isDrawing) {
+                    event.target.style.backgroundColor = "red";
+                }
+            });
+        }
     }
 }
+
+customGrid.addEventListener('click', function () {
+    const newGridSize = parseInt(prompt("Enter a new grid size (e.g., 16):"));
+    if (!isNaN(newGridSize)) {
+        gridSize = newGridSize; // Update the gridSize
+        createGrid(gridSize); // Recreate the grid with the new size
+    } else {
+        alert("Please enter a valid number for the grid size.");
+    }
+});
+
+function sideCalculator(gridSize) {
+    const result = 640 / gridSize;
+    return Math.round(result * 100) / 100;
+}
+
+createGrid(gridSize);
+
+// const customGrid = document.querySelector('#custom-grid');
+// customGrid.addEventListener('click', function () {
+//     const newGridSize = parseInt(prompt("Enter a new grid size (e.g., 16):"));
+//     if (!isNaN(newGridSize)) {
+//         gridSize = newGridSize; // Update the gridSize
+//         createGrid(gridSize); // Recreate the grid with the new size
+//     } else {
+//         alert("Please enter a valid number for the grid size.");
+//     }
+// });
